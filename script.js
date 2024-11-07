0dbd4615fa03ee01f8e634fcebed4b6a38093554
Francisco García Córdoba <ciscu6@gmail.com>
2024-11-07 19:17:20 +0100

    Agregado About

--- script.js
+++ script.js
@@ -1,16 +1,7 @@
-// Puedes añadir interactividad aquí si lo necesitas.
-document
-  .getElementById("contact-form")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
-    alert("Gracias por tu mensaje. ¡Te responderé pronto!");
-  });
-
 $(document).ready(() => {
   setInterval(() => {
     let containerFlip = $(".container-flip");
     let index = containerFlip.children().not(".d-none").index();
-    console.log(index);
     if (index >= containerFlip.children().length - 1) {
       index = 0;
     } else {
@@ -18,6 +9,5 @@ $(document).ready(() => {
     }
     containerFlip.children().addClass("d-none");
     $(containerFlip.children()[index]).removeClass("d-none");
-    console.log($(containerFlip.children()[index]));
   }, 3000);
 });
